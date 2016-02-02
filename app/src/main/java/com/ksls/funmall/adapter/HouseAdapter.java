42bd7546506bdbf4df6e3281ea2937a868bc6f49
bin.shen <bluelazysb@hotmail.com>
2016-02-02 16:04:12 +0800

    123213

---
+++ app/src/main/java/com/ksls/funmall/adapter/HouseAdapter.java
@@ -0,0 +1,30 @@
+package com.ksls.funmall.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ksls.funmall.R;
+import com.ksls.funmall.base.AppBaseAdapter;
+import com.ksls.funmall.base.AppQuery;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class HouseAdapter extends AppBaseAdapter {
+
+    public HouseAdapter(AppQuery aq, List<JSONObject> list) {
+        super(aq, list);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final JSONObject data = (JSONObject) list.get(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_house, null);
+        }
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/ksls/funmall/fragment/HouseFragment.java
+++ app/src/main/java/com/ksls/funmall/fragment/HouseFragment.java
@@ -5,11 +5,29 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.androidquery.callback.AjaxStatus;
 import com.ksls.funmall.R;
+import com.ksls.funmall.adapter.HouseAdapter;
+import com.ksls.funmall.base.AqArrayCallback;
 import com.ksls.funmall.base.BaseFragment;
+import com.ksls.funmall.base.Constants;
+import com.ksls.funmall.util.JSONUtil;
+import com.ksls.funmall.view.PullToRefreshView;
 
-public class HouseFragment extends BaseFragment {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HouseFragment extends BaseFragment implements PullToRefreshView.OnFooterRefreshListener {
+
+    private PullToRefreshView mPullToRefreshView;
+    private HouseAdapter mHouseAdapter;
+    private ListView mHouseList;
+    private List<JSONObject> house_list;
 
     @Nullable
     @Override
@@ -22,5 +40,47 @@ public class HouseFragment extends BaseFragment {
         super.onActivityCreated(savedInstanceState);
 
         initHeader();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        setUpViews();
+    }
+
+    @Override
+    public void onFooterRefresh(PullToRefreshView view) {
+
+    }
+
+    private void setUpViews() {
+        Integer user_id = appManager.getLoginUser().optInt("id");
+        house_list = new ArrayList<JSONObject>();
+        aq.request(Constants.API_BASE_URL + "/list_house/" + user_id, JSONArray.class, new AqArrayCallback<JSONArray>(aq) {
+            @Override
+            public void handleCallback(String url, JSONArray json, AjaxStatus status) {
+                showData(json);
+            }
+            @Override
+            public void afterCallback() {
+                progressDialog.dismiss();
+            }
+        });
+
+        mPullToRefreshView = (PullToRefreshView) getView().findViewById(R.id.pull_refresh_view_home);
+        mPullToRefreshView.setOnFooterRefreshListener(this);
+        mHouseList = (ListView) getView().findViewById(R.id.list_clients);
+    }
+
+    private void showData(JSONArray json) {
+        house_list.clear();
+        if(json != null && json.length() > 0) {
+            for (int i = 0; i < json.length(); i++) {
+                house_list.add(JSONUtil.getObject(json, i));
+            }
+            mHouseAdapter = new HouseAdapter(aq, house_list);
+            mHouseList.setAdapter(mHouseAdapter);
+        }
     }
 }
