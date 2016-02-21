f38bd5351492742ccee15275b711aa00e660ae6c
bin.shen <bluelazysb@hotmail.com>
2016-02-21 15:21:08 +0800

    123123

--- app/src/main/java/com/ksls/funmall/PortraitActivity.java
+++ app/src/main/java/com/ksls/funmall/PortraitActivity.java
@@ -3,11 +3,22 @@ package com.ksls.funmall;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class PortraitActivity extends Activity {
+import com.ksls.funmall.base.BaseActivity;
+
+public class PortraitActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portrait);
+
+        initHeader();
+        setUpViews();
+    }
+
+    private void setUpViews() {
+
+        String ticket = appManager.getLoginUser().optString("ticket");
+        aq.id(R.id.login_user_barcode).image("https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=" + ticket);
     }
 }
