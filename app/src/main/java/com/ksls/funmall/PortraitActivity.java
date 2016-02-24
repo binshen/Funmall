c3bb226fcfe886ca4b72f39cb89968cd6315394d
bin.shen <bluelazysb@hotmail.com>
2016-02-24 17:10:43 +0800

    123123

--- app/src/main/java/com/ksls/funmall/PortraitActivity.java
+++ app/src/main/java/com/ksls/funmall/PortraitActivity.java
@@ -1,9 +1,11 @@
 package com.ksls.funmall;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import com.ksls.funmall.base.BaseActivity;
+import com.ksls.funmall.util.TextViewUtil;
+
+import org.json.JSONObject;
 
 public class PortraitActivity extends BaseActivity {
 
@@ -18,7 +20,12 @@ public class PortraitActivity extends BaseActivity {
 
     private void setUpViews() {
 
-        String ticket = appManager.getLoginUser().optString("ticket");
+        JSONObject loginUser = appManager.getLoginUser();
+        String ticket = loginUser.optString("ticket");
         aq.id(R.id.login_user_barcode).image("https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=" + ticket, true, true, 0, R.drawable.icon_portrait);
+
+        TextViewUtil.setText(this, R.id.text_agency_company_name, loginUser.optString("company_name"));
+        TextViewUtil.setText(this, R.id.text_agency_company_address, loginUser.optString("address"));
+        TextViewUtil.setText(this, R.id.text_agency_company_contact, loginUser.optString("company_tel"));
     }
 }
